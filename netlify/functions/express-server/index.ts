55c1e2ce8d1c1b81f03bd56379d63456c8caad2b
Ndizihiwe Benny Chrispin <benny.ndizihiwe@amalitechtraining.org>
2025-01-20 00:46:08 +0200

    Configure Swagger for Netlify deployment

--- netlify/functions/express-server/index.ts
+++ netlify/functions/express-server/index.ts
@@ -1,8 +1,31 @@
 import serverless from "serverless-http";
 import dotenv from "dotenv";
 import app from "../../../src/app";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 dotenv.config();
 
-// Export the serverless handler for Netlify
+// Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task Force Pro API Documentation",
+      version: "1.0.0",
+      description: "API documentation for Task Force Pro",
+    },
+    servers: [
+      {
+        url: "/.netlify/functions/express-server",
+        description: "Netlify Serverless Function",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 export const handler = serverless(app);
